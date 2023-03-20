24bea0eec722673b7792282b3d74bd972f1dc0ed
Emmanuel-Mbaeze <mbaezesomto@gmail.com>
2023-03-20 11:02:59 -0700

    now

--- src/Components/Allitems/Allitems.jsx
+++ src/Components/Allitems/Allitems.jsx
@@ -4,11 +4,30 @@ import styled from "styled-components";
 const Allitems = () => {
   return (
     <Container>
-      <Wrapper></Wrapper>
+      <Wrapper>
+        <Card></Card>
+      </Wrapper>
     </Container>
   );
 };
 
 export default Allitems;
-const Wrapper = styled.div``;
-const Container = styled.div``;
+const Card = styled.div`
+  width: 100%;
+  height: 300px;
+  border: 1.9px solid #d975c0;
+  border-radius: 5px;
+`;
+const Wrapper = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
