c544c06adea012bd64b1e4ff87028d1dad41cda8
Emmanuel-Mbaeze <mbaezesomto@gmail.com>
2023-03-22 11:12:47 -0700

    image

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -4,57 +4,62 @@ import Reason from "./Reason";
 import GetStarted from "./GetStarted";
 
 const LandingPage = () => {
-    return (
-        <div>
-            <Container>
-                <Wrapper>
-                    <Hero>
-                        <Title>ANYONE CAN SELL ON CROSS AFRICA</Title>
-                        <Content>Become a merchant and sell to thousands of people across africa</Content>
-                        <Button>Start Selling Today</Button>
-                    </Hero>
-                    <Reason />
-                    <GetStarted />
-                </Wrapper>
-            </Container>
-        </div>
-    )
+  return (
+    <div>
+      <Container>
+        <Wrapper>
+          <Hero>
+            <Title>ANYONE CAN SELL ON CROSS AFRICA</Title>
+            <Content>
+              Become a merchant and sell to thousands of people across africa
+            </Content>
+            <Button>Start Selling Today</Button>
+          </Hero>
+          <Reason />
+          <GetStarted />
+        </Wrapper>
+      </Container>
+    </div>
+  );
 };
 
 export default LandingPage;
 
 const Button = styled.button`
-    padding: 15px 70px;
-    border: #D975C0;
-    background-color: #D975C0;
-    border-radius: 5px;
-    color: white;
-    font-weight: 600;
-    cursor: pointer;
+  padding: 15px 70px;
+  border: #d975c0;
+  background-color: #d975c0;
+  border-radius: 5px;
+  color: white;
+  font-weight: 600;
+  cursor: pointer;
 `;
 const Content = styled.div`
-    font-weight: 600;
-    font-size: 17px;
-    margin-bottom: 15px;
+  font-weight: 600;
+  font-size: 17px;
+  margin-bottom: 15px;
   text-align: center;
-`
+`;
 const Title = styled.div`
-    font-size: 35px;
-    font-weight: 700;
-    margin-bottom: 15px;
-    text-align: center;
-`
+  font-size: 35px;
+  font-weight: 700;
+  margin-bottom: 15px;
+  text-align: center;
+`;
 const Hero = styled.div`
-    /* width: 100%; */
-    background-image: linear-gradient(90deg, #d0ff00, #262626);
-    height: 600px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    color: white;
+  /* width: 100%; */
+  background-image: url("/Frame 114.png");
+  height: 600px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: white;
+  opacity: 0.8;
+  -webkit-backdrop-filter: blur(5px);
+  backdrop-filter: blur(5px);
 `;
 const Wrapper = styled.div``;
 const Container = styled.div`
-    width: 100%;
-`
+  width: 100%;
+`;
