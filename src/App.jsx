168c3a322872cde0cbf021c6f77eb823fce68572
Davidokih <davidyole023@gmail.com>
2023-05-11 05:39:24 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,15 @@ import LandingPage from "./Components/LandingPage/LandingPage";
 import PageRouting from "./Components/PageRouting";
 import Private from "./Components/Private";
 import SellerRouting from "./Components/SellerRouting";
-import AdminRoutePage from "./Components/AdninRoutePage"
-import SignInAdmin from "./Components/Admin/SignInAdmin"
-import SignUpAdmin from "./Components/Admin/SignUpAdmin"
-import AdminPrivate from "./Components/AdminPrivate"
-import AdminOtp from "./Components/Admin/AdminOtp"
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/*" element={ <PageRouting />} />
-        {/* <Route path="/admin-dashboard/*" element={ <AdminPrivate><AdminRoutePage /></AdminPrivate>} /> */}
         <Route path="/seller-page" element={ <LandingPage />} />
         <Route path="/seller-dashboard/*" element={ <Private><SellerRouting /></Private>} />
-        {/* <Route path="/login-admin" element={ <SignInAdmin /> } />
-        <Route path="/signup-admin" element={ <SignUpAdmin /> } />
-        <Route path="/otp-admin" element={ <AdminOtp /> } /> */}
       </Routes>
-      
-     
     </div>
   );
 };
--- src/Components/AdminPrivate.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const Private = ({ children }) => {
-    const user = JSON.parse(localStorage.getItem("user"));
-
-    return user?.isAdmin === true ? children : <Navigate to="/login-admin" />;
-};
-
-export default Private;
